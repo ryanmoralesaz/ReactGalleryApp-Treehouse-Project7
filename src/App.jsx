fa1c7686bb4f9217fb0a28a3c3259754cdbe4061
WestMecRyan <129911377+WestMecRyan@users.noreply.github.com>
2024-05-27 11:41:43 -0700

    got fetch working for animals

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,11 @@ import NotFound from './components/NotFound';
 
 function App() {
   const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [topic, setTopic] = useState('cats');
 
   const fetchData = async (query) => {
+    setLoading(true);
     const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
 
     try {
@@ -25,17 +28,62 @@ function App() {
     } catch (error) {
       console.error('Error fetching and parsing data', error);
     }
+    setLoading(false);
   };
+
+  useEffect(() => {
+    fetchData(topic); // Default fetch for the initial load
+  }, [topic]);
+
   return (
     <div className='App'>
       <Nav />
       <Search />
       <Routes>
         <Route path='/' element={<Navigate replace to='/cats' />} />
-        <Route path='/cats' element={<PhotoList topic='cats' />} />
-        <Route path='/dogs' element={<PhotoList topic='dogs' />} />
-        <Route path='/computers' element={<PhotoList topic='computers' />} />
-        <Route path='/search/:query' element={<PhotoList />} />
+        <Route
+          path='/cats'
+          element={
+            <PhotoList
+              topic='cats'
+              photos={photos}
+              loading={loading}
+              setTopic={setTopic}
+            />
+          }
+        />
+        <Route
+          path='/dogs'
+          element={
+            <PhotoList
+              topic='dogs'
+              photos={photos}
+              loading={loading}
+              setTopic={setTopic}
+            />
+          }
+        />
+        <Route
+          path='/computers'
+          element={
+            <PhotoList
+              topic='computers'
+              photos={photos}
+              loading={loading}
+              setTopic={setTopic}
+            />
+          }
+        />
+        <Route
+          path='/search/:query'
+          element={
+            <PhotoList
+              photos={photos}
+              loading={loading}
+              setTopic={setTopic}
+            />
+          }
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
